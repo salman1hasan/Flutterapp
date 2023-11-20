39824660a74c0b59bef0ff8138f86c6583f878a1
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-20 08:47:56 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,24 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: 
+              colors: [
+                Color.fromARGB(255, 228, 153, 15),
+                Color.fromARGB(255, 10, 10, 10),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text(
+              'UNGUARDED',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            ),
           ),
         ),
       ),
